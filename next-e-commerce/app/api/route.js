373399f3e3c0c09c9f7c68ca2dfe55ef2103165c
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2025-01-09 19:26:48 +0000

    user model created

--- next-e-commerce/app/api/route.js
+++ next-e-commerce/app/api/route.js
@@ -3,3 +3,6 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
     return NextResponse.json({time: new Date().toLocaleString() });
 }
+
+
+//mongodb+srv://nxus:Ba3hBJugekwxkhmR@cluster0.zgmij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
