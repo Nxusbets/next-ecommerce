6d3a78d6189de56ab39473fa7548af44d54949a3
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2025-01-09 18:42:40 +0000

    navigation, api creatin

---
+++ next-e-commerce/components/TopNav.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function TopNav() {
+
+    return (
+        <nav className="nav shadow p-2 justify-content-between mb-3">
+            <Link href="/" className="nav-link">Next E-commerce</Link>
+
+            <div className='d-flex'>
+                <Link href="/login" className='nav-link'>Login</Link>
+            </div>
+
+            <div className='d-flex'>
+                <Link href="/register" className='nav-link'>Register</Link>
+            </div>
+
+
+        </nav>
+    )
+}
